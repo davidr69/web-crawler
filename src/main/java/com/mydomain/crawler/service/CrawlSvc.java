4f8c6698e29eb043aa514bccde750a601ffd50be
drosario69 <david@lavacro.com>
2020-10-29 02:47:10 -0400

    Finishing documentation

--- src/main/java/com/mydomain/crawler/service/CrawlSvc.java
+++ src/main/java/com/mydomain/crawler/service/CrawlSvc.java
@@ -1,7 +1,6 @@
 package com.mydomain.crawler.service;
 
 import com.mydomain.crawler.CrawlerException;
-import com.mydomain.crawler.MyThread;
 import com.mydomain.crawler.model.RadixTree;
 
 import org.slf4j.Logger;
@@ -12,23 +11,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
 
 @Component
 public class CrawlSvc {
 	private static final Logger logger = LoggerFactory.getLogger(CrawlSvc.class);
 	private final ParseSvc parseSvc;
-	private Integer activeThreads = 0;
-	private ExecutorService executorService;
 	private RadixTree tree;
 	private String domain;
-	// concurrency stuff
-	private final Object mutex = new Object();
-	private Lock lock = new ReentrantLock();
 
 	@Value("${worker.threads}")
 	private Integer THREADS;
@@ -41,7 +30,6 @@ public class CrawlSvc {
 
 	public void workflow(String domain) {
 		logger.info("Workflow started");
-		executorService = Executors.newFixedThreadPool(THREADS, new MyThread());
 		this.domain = domain;
 
 		List<String> anchors = null;
@@ -55,59 +43,18 @@ public class CrawlSvc {
 		tree = new RadixTree(domain);
 		// farm out threads
 		work(anchors);
+		tree.display();
 	}
 
 	private void work(List<String> urls) {
-		logger.info("work ... threads: {}", activeThreads);
+		logger.info("work ...");
 		// get home page
 		for(String uri: urls) {
 			try {
 				// multiple threads might try to update the tree, so ...
-				boolean success = false;
-				synchronized (mutex) {
-					success = tree.put(uri);
-				}
-				if(success && uri.startsWith(domain)) {
+				if(tree.put(uri) && uri.startsWith(domain)) {
 					logger.info("process {}", uri);
-					lock.lock();
-					if(activeThreads == THREADS) {
-						lock.unlock();
-						while(true) {
-							try {
-								logger.info("sleeping ...");
-								Thread.sleep(2000);
-							} catch(InterruptedException e) {
-								// noop
-							}
-							lock.lock();
-							if(activeThreads < THREADS) {
-								activeThreads++; // going to reserve a spot
-								lock.unlock();
-								break;
-							}
-							lock.unlock();
-						}
-					} else {
-						activeThreads++;
-						lock.unlock();
-					}
-					if(activeThreads < THREADS) {
-						Callable<Integer> task = () -> {
-							try {
-								work(parseSvc.parseUriToDocument(uri));
-								lock.lock();
-								activeThreads--;
-								lock.unlock();
-							} catch(CrawlerException e) {
-								logger.error("Thread error: {}", e.getMessage());
-								return 1;
-							}
-							return 0;
-						};
-						executorService.submit(task);
-					} else {
-						logger.error("Too many threads");
-					}
+					work(parseSvc.parseUriToDocument(uri));
 				}
 			} catch(CrawlerException e) {
 				logger.error("Radix tree error: {}", e.getMessage());
